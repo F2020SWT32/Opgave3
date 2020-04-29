d917735ca042875c0ba5872e898189aeee4c7fdc
Rasmus Carlsen <rasmus.carlsen@web.de>
2020-04-29 14:49:44 +0200

    Added testcases and tests for extensions

--- Microwave.Test.Integration/ST1_TimerTests.cs
+++ Microwave.Test.Integration/ST1_TimerTests.cs
@@ -28,19 +28,83 @@ namespace Microwave.Test.Integration
             _uut = new CookController(_timer, _display, _powerTube, _ui);
         }
 
-        [Test]
-        public void CookControllerTest()
+        [TestCase(0, 1)]
+        [TestCase(50, 1)]
+        [TestCase(0, 3)]
+        [TestCase(700, 3)]
+        public void CookControllerTest(int power, int seconds)
         {
-            _uut.StartCooking(0, 1000);
-            Thread.Sleep(1000);
+            _uut.StartCooking(0, 1000*seconds);
+            Thread.Sleep(1000*seconds);
 
             Received.InOrder(() =>
             {
-                _powerTube.TurnOn(0);
-                _display.ShowTime(0, 0);
+                _powerTube.TurnOn(power);
+
+                for (int i = seconds - 1; i >= 0; i++)
+                    _display.ShowTime(i / 60, i % 60);
+
                 _powerTube.TurnOff();
                 _ui.CookingIsDone();
             });
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(50, 1)]
+        [TestCase(0, 3)]
+        [TestCase(700, 3)]
+        public void CookControllerTestExt3(int power, int seconds)
+        {
+            _uut.StartCooking(0, 1000 * seconds);
+
+            int abortAfterms = (1000 * seconds) / 2;
+
+            Thread.Sleep(abortAfterms);
+
+            _uut.Stop();
+
+            Thread.Sleep((seconds * 1000) - abortAfterms);
+
+            Received.InOrder(() =>
+            {
+                _powerTube.TurnOn(power);
+
+                for (int i = (abortAfterms/1000) - 1; i >= 0; i++)
+                    _display.ShowTime(i / 60, i % 60);
+
+                _powerTube.TurnOff();
+            });
+
+
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(50, 1)]
+        [TestCase(0, 3)]
+        [TestCase(700, 3)]
+        public void CookControllerTestExt4(int power, int seconds)
+        {
+            _uut.StartCooking(0, 1000 * seconds);
+
+            int abortAfterms = (1000 * seconds) / 2;
+
+            Thread.Sleep(abortAfterms);
+
+            _uut.Stop();
+
+            Thread.Sleep((seconds*1000) - abortAfterms);
+
+            Received.InOrder(() =>
+            {
+                _powerTube.TurnOn(power);
+
+                for (int i = (abortAfterms / 1000) - 1; i >= 0; i++)
+                    _display.ShowTime(i / 60, i % 60);
+
+                _powerTube.TurnOff();
+            });
+
+
         }
     }
 }
